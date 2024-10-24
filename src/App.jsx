b10b663297d951407e04c018fbf087020c9d320e
C14Vilches <fvilches93@hotmail.com>
2024-10-24 19:22:46 -0300

    agregado useId en InputField, handleFormSubmit funcionando, modificado FormStyle.css 24/10/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
 import Drawer from "./Components/Drawer/Drawer";
-import Banner from "./Components/Banner/Banner";
-import Calendar from "./Components/Calendar/Calendar";
+import { useState } from "react";
+import FormControl from "./Components/Form/FormControl";
+import InputField from "./Components/Form/InputField";
 function App() {
-  const [input, setInput] = useState("valor inicial");
-  const [textArea, setTextArea] = useState("valor secundario");
-  const handleFormSubmit = (e) => {
-    console.log(e);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleFormSubmit = () => {
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    console.log(data);
   };
   return (
     <>
       <Drawer>
-        <Calendar />
+        <FormControl handleFormSubmit={handleFormSubmit}>
+          <InputField
+            type="text"
+            name="nombre"
+            placeholder={"Ingresa tu nombre"}
+            value={name}
+            handleChange={setName}
+            required={true}
+          />
+          <InputField
+            type="email"
+            name="correo"
+            placeholder={"Ingresa tu correo"}
+            value={email}
+            handleChange={setEmail}
+            required={true}
+          />
+          <InputField
+            type="password"
+            name="contraseña"
+            placeholder={"Ingresa tu contraseña"}
+            value={password}
+            handleChange={setPassword}
+            required={true}
+          />
+        </FormControl>
       </Drawer>
     </>
   );
--- src/Components/Form/FormControl.jsx
+++ src/Components/Form/FormControl.jsx
@@ -1,12 +1,14 @@
 export default function FormControl({ children, handleFormSubmit }) {
   const handleSubmit = (event) => {
     event.preventDefault();
-    handleFormSubmit(event.target);
+    handleFormSubmit(event);
   };
   return (
     <div className="form_control">
-      <form onSubmit={handleSubmit}>{children}</form>
-      <button type="submit">enviar</button>
+      <form onSubmit={handleSubmit}>
+        {children}
+        <button type="submit">Enviar formulario</button>
+      </form>
     </div>
   );
 }
