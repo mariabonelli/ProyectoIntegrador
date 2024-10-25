beac4788224f5b9c38b73a9f79fae8e9604dee8e
belenvidal1 <belenpaz14@gmail.com>
2024-10-25 17:30:10 -0300

    integracion de axios en el login 25/10/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import Drawer from "./Components/Drawer/Drawer";
 import Departamentos from "./Pages/Administrador/Departamentos";
@@ -7,6 +9,28 @@ import ListadoDeServicios from "./Pages/ListadoDeServicios";
 import Login from "./Components/Login/Login";
 
 function App() {
+  const [user, setUser] = useState(null)
+
+
+  useEffect(()=>{ 
+    const body={
+      "email": "unicajefa@renca.cl",
+      "password": "fun123"
+  }
+    //axios.get("URL").then().catch()
+    //axios.post("URL", {body(que es un objeto)}).then().catch()
+    axios.post("http://localhost:8080/api/auth/login", body)
+    .then((resp)=>{
+      //console.log(resp.data)
+      setUser(resp.data)
+      localStorage.setItem("user", JSON.stringify(resp.data))
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }, [])
+
+
   return (
     <>
       <Drawer>
@@ -17,7 +41,7 @@ function App() {
           <Route path="/" element={<ListadoDeServicios />} />
         </Routes>
       </Drawer>
-      <Login />
+      {!user&&<Login />}
     </>
   );
 }
