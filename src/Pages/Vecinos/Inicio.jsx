8951ed71dee8a24164da2df1e0caf219e46f5c25
EstefanyRodriguezP <nicol.na14@gmail.com>
2024-10-28 15:49:51 -0300

    actualización vistas inicio vecino, inicio funcionario y otras consultas 28/10/24

--- src/Pages/Vecinos/Inicio.jsx
+++ src/Pages/Vecinos/Inicio.jsx
@@ -1,6 +1,43 @@
 /* PatiDevs */
+import List from "../../Components/List/List";
+import ListItemTitle from "../../Components/List/ListItemTitle";
+import "./InicioStyle.css";
+
 function Inicio() {
-  return <div>Inicio</div>;
+  return (
+    <div className="container_inicio_vecino">
+      <List>
+        <ListItemTitle subtitle={"Gestiona trámites para la mascota"}>
+          Veterinaria
+        </ListItemTitle>
+      </List>
+      <List>
+        <ListItemTitle subtitle={"Gestiona trámites para áreas verdes"}>
+          Áreas Verdes
+        </ListItemTitle>
+      </List>
+      <List>
+        <ListItemTitle subtitle={"Revisa el proceso de los trámites"}>
+          Estado de trámites
+        </ListItemTitle>
+      </List>
+      <List>
+        <ListItemTitle subtitle={"Revisa los trámites finalizados"}>
+          Historial trámites finalizados
+        </ListItemTitle>
+      </List>
+      <List>
+        <ListItemTitle subtitle={"Ver otras consultas de los usuarios"}>
+          Otras consultas
+        </ListItemTitle>
+      </List>
+      <List>
+        <ListItemTitle subtitle={"Revisa el historial de tus notificaciones"}>
+          Historial de notificaciones
+        </ListItemTitle>
+      </List>
+    </div>
+  );
 }
 
 export default Inicio;
