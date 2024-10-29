f5e8b5bf58775dde34b4ab05ab247fe759bad2f5
Znatan13 <duran.maty@gmail.com>
2024-10-29 10:46:26 -0300

    Añadido de funcion axios a Listado tramite y agretramite funcionario 29/10/24

--- src/Pages/Funcionarios/AgregarTramite.jsx
+++ src/Pages/Funcionarios/AgregarTramite.jsx
@@ -1,5 +1,6 @@
 /* PatiDevs */
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import FormControl from "../../Components/Form/FormControl";
 import InputField from "../../Components/Form/InputField";
 import TextArea from "../../Components/Form/TextArea";
@@ -9,6 +10,8 @@ function AgregarTramite() {
   const [nombre, setNombre] = useState("");
   const [descripcion, setDescripcion] = useState("");
 
+  const navigate = useNavigate();
+
   const handleSubmit = () => {
     const body = {
       nombre: nombre,
@@ -17,7 +20,7 @@ function AgregarTramite() {
       .post(`http://localhost:8080/api/tramites/nuevo/1`, body)
       .then((response) => {
         if (response) {
-          console.log(response);
+          navigate("/listadotramites");
         }
       })
       .catch((error) => {
--- src/Pages/Funcionarios/ListadoTramites.jsx
+++ src/Pages/Funcionarios/ListadoTramites.jsx
@@ -1,5 +1,27 @@
+/* PatiDevs */
+import { useEffect, useState } from "react";
+import axios from "axios";
+import List from "../../Components/List/List";
+import ListItemTitle from "../../Components/List/ListItemTitle";
+
 function ListadoTramites() {
-  return "ListadoTramites";
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get(`http://localhost:8080/api/tramites/lista`).then((response) => {
+      if (response) {
+        setData(response.data);
+      }
+    });
+  }, []);
+  return (
+    <div style={{ margin: "20px auto", maxWidth: "100%" }}>
+      {data.map((item) => (
+        <List key={item.id}>
+          <ListItemTitle>{item.nombre}</ListItemTitle>
+        </List>
+      ))}
+    </div>
+  );
 }
 
 export default ListadoTramites;
