c672b1eaedb97c939928513d972ebc0387dbfaa0
unknown <nadimalopez@gmail.com>
2024-10-29 11:41:05 -0300

    Avance formulario registro en login 29/10/24

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -55,5 +55,103 @@ function Login({ loginIsSuccess }) {
     </div>
   );
 }
+function Signup() {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [email, setEmail] = useState("");
+  const [password,setPassword] = useState("");
 
+  const handleSubmit = () => {
+    const body = {
+      nombre: nombre,
+      apellido: apellido,
+      telefono: telefono,
+      direccion: direccion,
+      email: email,
+      password: password,
+
+    };
+    axios
+    .post("localhost:8080/api/auth/registro", body)
+    .then((resp)) => {
+
+    }
+  }
+  return (
+    <div className="signup_container">
+      <h2>Registro</h2>
+      <FormControl  handleFormSubmit={handleSubmit}>
+        <InputField
+          placeholder={"Nombre"}
+          type={"text"}
+          outlined={true}
+          name={"nombre"}
+          value={nombre}
+          handleChange={setNombre}
+        />
+        <InputField
+          placeholder={"Apellido"}
+          type={"text"}
+          outlined={true}
+          name={"apellido"}
+          value={apellido}
+          handleChange={setApellido}
+        />
+        <InputField
+          placeholder={"Teléfono"}
+          type={Number}
+          outlined={true}
+          name={"telefono"}
+          value={telefono}
+          handleChange={setTelefono}
+        />
+        <InputField
+          placeholder={"Dirección"}
+          type={"text"}
+          outlined={true}
+          name={"direccion"}
+          value={direccion}
+          handleChange={setDireccion}
+        />
+        <InputField
+          placeholder={"Correo Electrónico"}
+          type={"email"}
+          outlined={true}
+          name={"email"}
+          value={email}
+          handleChange={setEmail}
+        />
+        <InputField
+          placeholder={"Confirmar correo electrónico"}
+          type={"email"}
+          outlined={true}
+          name={"email"}
+          value={email}
+          handleChange={setEmail}
+        />
+        <InputField
+          placeholder={"Contraseña"}
+          type={"password"}
+          outlined={true}
+          name={"password"}
+          value={password}
+          handleChange={setPassword}
+        />
+      
+      <InputField
+          placeholder={"Confirmar contraseña"}
+          type={"password"}
+          outlined={true}
+          name={"password"}
+          value={password}
+          handleChange={setPassword}
+        />
+      </FormControl>
+
+
+    </div>
+  )
+}
 export default Login;
