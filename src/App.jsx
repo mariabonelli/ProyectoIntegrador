114ea9f2f0e2496d337de29ec01ee645aac610f1
EstefanyRodriguezP <nicol.na14@gmail.com>
2024-10-30 09:56:58 -0300

    actualización inicio vecino 30/10/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+
 import Drawer from "./Components/Drawer/Drawer";
+
 import Departamentos from "./Pages/Administrador/Departamentos";
+
 import Servicios from "./Pages/Administrador/Servicios";
+
 import Solicitudes from "./Pages/Administrador/Solicitudes";
+
 import ListadoDeServicios from "./Pages/ListadoDeServicios";
+
 import InicioVecino from "./Pages/Vecinos/Inicio";
+
 import InicioFuncionario from "./Pages/Funcionarios/Inicio";
+
 import OtrasConsultas from "./Pages/Vecinos/OtrasConsultas";
+
 import TramitePorServicio from "./Pages/Vecinos/TramitePorServicio";
+
 import AgregarTramite from "./Pages/Funcionarios/AgregarTramite";
 import ListadoTramites from "./Pages/Funcionarios/ListadoTramites";
-<<<<<<< HEAD
-import EstadoTramiteFuncionario from "./Pages/Funcionarios/EstadoTramite";
-import EstadoTramiteVecino from "./Pages/Vecinos/EstadoTramite";
-=======
 import ListadoTramitesFinalizados from "./Pages/Funcionarios/ListadoTramiteFinalizado";
->>>>>>> 3f38815357a36282d6393ad0b4f423032026009a
+import EstadoTramite from "./Pages/Vecinos/EstadoTramite";
 
 function App() {
   return (
@@ -27,25 +33,16 @@ function App() {
           <Route path="/solicitudes" element={<Solicitudes />} />
           <Route path="/" element={<InicioVecino />} />
           <Route path="/listadotramites" element={<ListadoTramites />} />
-<<<<<<< HEAD
-          <Route path="/agregartramites" element={<AgregarTramite />} />
-          <Route
-            path="/agregartramites/item/:id"
-            element={<AgregarTramite />}
-          />
-          <Route
-            path="/vecino/estadotramites"
-            element={<EstadoTramiteVecino />}
-          />
-          <Route
-            path="/funcionario/estadotramites"
-            element={<EstadoTramiteFuncionario />}
-=======
           <Route
             path="/listadotramitesfinalizados"
             element={<ListadoTramitesFinalizados />}
->>>>>>> 3f38815357a36282d6393ad0b4f423032026009a
           />
+          <Route path="/otrasconsultas" element={<OtrasConsultas />}></Route>
+          <Route
+            path="/tramiteporservicio"
+            element={<TramitePorServicio />}
+          ></Route>
+          <Route path="/estadotramites" element={<EstadoTramite />}></Route>
         </Routes>
       </Drawer>
     </>
