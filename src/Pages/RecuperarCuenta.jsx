d898a7659ca7110f542a74c8a13829aaed769401
BrancoMI <branco.andrei.moreira@gmail.com>
2024-10-30 13:01:22 -0300

    recuperarCuenta actualizada 30-10-24

--- src/Pages/RecuperarCuenta.jsx
+++ src/Pages/RecuperarCuenta.jsx
@@ -1,29 +1,36 @@
 /* OneBits */
-import React, {useState} from "react";
+import React, { useState } from "react";
 import FormControl from "../Components/Form/FormControl";
+import Button from "../Button/Button";
 
 function RecuperarCuenta() {
-  const [mensaje, setMensaje] = useState ('');
-  
+  const [mensaje, setMensaje] = useState("");
+
   const handleFormSubmit = (form) => {
     const email = form.email.value;
 
     setMensaje(`Se ha enviado un enlace de recuperación a ${email}`);
     form.reset();
   };
-return <div>
-    <h1>RecuperarCuenta</h1>
-    <FormControl handleFormSubmit={handleFormSubmit}>
-      <label>
-        correo Electronico:
-        <input
-        type="email"
-        name="email"
-        required
-        />
-      </label>
-    </FormControl>
-  </div>;
+
+  return (
+    <div>
+      <h1>RecuperarCuenta</h1>
+      <FormControl handleFormSubmit={handleFormSubmit}>
+        <label>
+          correo Electronico:
+          <input type="email" name="email" required />
+        </label>
+        <div>
+          <Button
+            handleClick={(_) => setSignup(true)}
+            variant="btn_small btn_outlined btn_secondary "
+          >
+            Aceptar
+          </Button>
+        </div>
+      </FormControl>
+    </div>
+  );
 }
 export default RecuperarCuenta;
-
