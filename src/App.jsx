59707c9006f879de29a61c7cca5fecabcc6d27dc
FelipeBascunanA <felipe.bascunan22@gmail.com>
2024-11-04 14:41:12 -0300

    Actualizacion Componente y vistas React 04/11

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Drawer from "./Components/Drawer/Drawer";
 import Departamentos from "./Pages/Administrador/Departamentos";
 import Servicios from "./Pages/Administrador/Servicios";
 import Solicitudes from "./Pages/Administrador/Solicitudes";
 import ListadoDeServicios from "./Pages/ListadoDeServicios";
+import HistorialDeNotificaciones from "./Pages/HistorialDeNotificaciones";
+import ListadoDePlantillas from "./Pages/Funcionarios/ListadoDePlantillas";
+import ActualizacionDePlantillas from "./Pages/Funcionarios/ActualizacionDePlantilla";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/notificaciones/lista")
+      .then((resp) => {
+        console.log(resp.data);
+        setData(resp.data.results);
+      })
+      .catch((error) => {});
+  }, []);
   return (
     <>
       <Drawer>
@@ -14,6 +30,15 @@ function App() {
           <Route path="/servicios" element={<Servicios />} />
           <Route path="/solicitudes" element={<Solicitudes />} />
           <Route path="/" element={<ListadoDeServicios />} />
+          <Route
+            path="/historialnoti"
+            element={<HistorialDeNotificaciones />}
+          />
+          <Route path="/listadoplantillas" element={<ListadoDePlantillas />} />
+          <Route
+            path="/actuplantilla"
+            element={<ActualizacionDePlantillas />}
+          />
         </Routes>
       </Drawer>
     </>
---
+++ src/Components/Timeline/TimelineContent.jsx
@@ -0,0 +1,8 @@
+import { Children } from "react";
+import "./TimelineStyle.css";
+
+function Timeline({ children }) {
+  return <div className="content_timeline">{children}</div>;
+}
+
+export default Timeline;
